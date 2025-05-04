854e98420fa033a26bc3ce7f7bac42475810f64f
akrabatt <starwars-1999@yandex.ru>
2025-05-04 02:38:18 +0200

    add CMake

---
+++ task_6_data_sort_strategy/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "headers/class_temp.h"
+#include "headers/class_number_processro.h"
+#include "headers/class_abs_sort.h"
+#include "headers/class_def_sort.h"
+#include "headers/class_rev_sort.h"
+
+int main()
+{
+	NumberProcessor sort_machine;	
+	
+	// default sort
+	sort_machine.addNumber(5);
+	sort_machine.addNumber(3);
+	sort_machine.addNumber(8);
+	sort_machine.addNumber(-3);
+	sort_machine.addNumber(7);
+	sort_machine.addNumber(2);
+	sort_machine.addNumber(-6);
+	// print before sort
+	std::cout << "Before default sorte: ";
+	sort_machine.print();	
+	// set default strategy
+	sort_machine.setStrategy(std::make_unique<DefaultSort<int>());
+	sort_machine.process();
+	// print after default sorting
+	std::cout << "After default sorte: ";
+	sort_machine.print();
+}
