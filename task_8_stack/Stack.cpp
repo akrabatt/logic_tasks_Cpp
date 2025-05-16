ed31d658db845094d1459ed656cfc94c5870f7de
akrabatt <starwars-1999@yandex.ru>
2025-05-16 17:43:19 +0200

    end stack method

--- task_8_stack/Stack.cpp
+++ task_8_stack/Stack.cpp
@@ -8,9 +8,12 @@ bool Stack::checkBracketStackMethod(std::vector<char>& brackets)
 	{
 		if((artifact.empty()) && ((it == ca) || (it == cc))) { return false; }
 		if(artifact.empty()) { artifact.push(it); continue; }
-		
-			
-
+		if((artifact.top() == oa && it == ca) || (artifact.top() == oc && it == cc))
+		{
+			artifact.pop();
+		} else {
+			artifact.push(it);
+		}
 	}
 
 	return artifact.empty();
--- task_8_stack/main.cpp
+++ task_8_stack/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <vector>
 #include "Stack.h"
 
 int main()
 {
+	Stack st_obj;
+	std::vector<char> brackets = { '(', ')', '(', '{', '}', ')', '(', ')' };
 
+	bool result = st_obj.checkBracketStackMethod(brackets);
+	std::cout << "result: " << result << std::endl;
 }
